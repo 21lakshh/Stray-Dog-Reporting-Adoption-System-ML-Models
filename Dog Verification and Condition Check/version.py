60917df3d6d2b29e86d7a8a540137ebbfea91937
Lakshya Paliwal <lakshyapaliwalodd@gmail.com>
2025-03-20 13:56:54 +0530

    Add files via upload

---
+++ Dog Verification and Condition Check/version.py
@@ -0,0 +1,11 @@
+import fastapi
+import PIL
+import os
+import logging
+import base64
+import requests
+import io
+
+print(f"FastAPI Version: {fastapi.__version__}")
+print(f"PIL Version: {PIL.__version__}")
+print(f"Requests Version: {requests.__version__}")
