4a21b2a71d168607fa9f077f0ebe7384a206671f
Lakshya Paliwal <lakshyapaliwalodd@gmail.com>
2025-03-20 13:57:17 +0530

    Update version.py

--- Dog Verification and Condition Check/version.py
+++ Dog Verification and Condition Check/version.py
@@ -1,10 +1,7 @@
 import fastapi
 import PIL
-import os
-import logging
-import base64
 import requests
-import io
+
 
 print(f"FastAPI Version: {fastapi.__version__}")
 print(f"PIL Version: {PIL.__version__}")
